5420b6a7d3a200c8a4f51b132445acf19f1b9dbb
LuigiZannella <zannellaluigi@outlook.com>
2022-06-28 02:14:52 +0200

    Esercizio finito

--- src/jana60/Utente.java
+++ src/jana60/Utente.java
@@ -1,5 +1,83 @@
 package jana60;
 
 public class Utente {
+	 private String nome;
+	    private String cognome;
+	    private String email;
+	    private String password;
+	    private int age;
+
+	    //constr
+
+	    public Utente(String nome, String cognome, String email, String password, int age) throws Exception {
+
+	        if ( !(email.contains("@")) || !(email.contains(".") ))
+	            throw new Exception("La mail deve contenere un '@' e un '.'");
+	        if ( password.length()<8 || password.length()>12)
+	            throw new Exception("La password deve contenere tra gli 8 e i 12 caratteri");
+	        if (age<18)
+	            throw new Exception("Devi avere più di 18 anni");
+
+	        this.nome = nome;
+	        this.cognome = cognome;
+	        this.email = email;
+	        this.password = password;
+	        this.age = age;
+	    }
+
+
+	    //getter/setter
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public String getCognome() {
+	        return cognome;
+	    }
+
+	    public void setCognome(String cognome) {
+	        this.cognome = cognome;
+	    }
+
+	    public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) throws Exception {
+	        if ( password.length()<8 || password.length()>12)
+	            throw new Exception("La mail deve contenere un '@' e un '.'");
+	        this.email = email;
+	    }
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) throws Exception {
+	        if ( !(email.contains("@")) || !(email.contains(".") ))
+	            throw new Exception("La password deve contenere tra gli 8 e i 12 caratteri");
+	        this.password = password;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) throws Exception {
+	        if (age<18)
+	            throw new Exception("Devi avere più di 18 anni");
+	        this.age = age;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Utente con nome: " + nome +", cognome: " + cognome +" ed età di: "+age+". Email: " + email +
+	                ", password: " + password +". Grazie per esserti registrato";
+	    }
 
 }
